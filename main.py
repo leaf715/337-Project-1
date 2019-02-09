2c0ec3e8fb8b51b318f1d7aa5e1b79aacfab3ceb
leaf715 <shuhan1998@gmail.com>
2019-02-08 19:33:50 -0600

    adds code that finds hosts

--- main.py
+++ main.py
@@ -1,7 +1,12 @@
 import json
+import nltk
+from nltk.corpus import stopwords, names
+stop = stopwords.words('english')
+# http://www.nltk.org/
+import re
+# https://docs.python.org/3/library/re.html
 
 def main():
-    print("running")
     cfg_file = open("award_show.config", "r")
     cfg_lines = cfg_file.readlines()
     data_path = json.loads(cfg_lines[0])
@@ -10,8 +15,47 @@ def main():
     raw_tweets = json.loads(raw_data.read())
     tweets = []
     for tweet in raw_tweets:
-        tweets.append(tweet["text"])
-    print(tweets[0])
+        text = tweet["text"].split()
+        stripped_text = " ".join(word for word in text if word[0] is not '#' and word[0] is not '@')
+        stripped_text = stripped_text.replace('\'s','')
+        stripped_text = stripped_text.replace('.','')
+        stripped_text = stripped_text.replace(',','')
+        stripped_text = stripped_text.replace('Golden Globes','')
+        if stripped_text.find('RT') != -1:
+            stripped_text = stripped_text[:stripped_text.find('RT')]
+        tweets.append(stripped_text)
+    host_tweets = get_relevant_tweets(["host"], tweets)
+    potential_hosts = get_names(host_tweets)
+    potential_hosts = sorted(potential_hosts.items(), key = lambda x: x[1], reverse=True)
+    if potential_hosts[1][1] - potential_hosts[0][1] < 100:
+        print('CoHosts: '+potential_hosts[0][0]+' and '+potential_hosts[1][0])
+    else:
+        print('Host: '+potential_hosts[0][0])
+
+def get_relevant_tweets(keys, tweets):
+    relevant = []
+    for tweet in tweets:
+        for key in keys:
+            if re.search(key, tweet, re.IGNORECASE):
+                relevant.append(tweet)
+                break
+    return relevant
+
+def get_names(tweets):
+    names = {}
+    for tweet in tweets:
+        words = tweet.split()
+        for i in range(len(words)):
+            if words[i][0].isupper():
+                if i + 1 < len(words) - 1:
+                    if words[i+1][0].isupper():
+                        name = (words[i] + ' ' + words[i+1])
+                        if name in names.keys():
+                            names[name] = names[name] + 1
+                        else:
+                            names[name] = 1
+
+    return names
 
 if __name__ == "__main__":
     main()
