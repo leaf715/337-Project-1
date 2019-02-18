d1b579a69c5cc4a4f0ba2e880d22bdd3d19c9db2
leaf715 <shuhan1998@gmail.com>
2019-02-17 23:25:23 -0600

    updates readme

--- README.md
+++ README.md
@@ -1,1 +1,11 @@
 # 337-Project-1
+Group 14
+Brennan Adler Joe Chookaszian Shu Han
+Dependencies:
+re
+nltk
+copy
+json
+To Run:
+adjust hardcoded award names and file name in award_show.config
+run python3 gg_tweet_parser.py
---
+++ gg_api.py
@@ -0,0 +1,83 @@
+'''Version 0.35'''
+import json
+import nltk
+import copy
+from gg_program import gg_parser
+from nltk.corpus import stopwords, names
+stop = stopwords.words('english')
+# http://www.nltk.org/
+import re
+# https://docs.python.org/3/library/re.html
+
+OFFICIAL_AWARDS_1315 = ['cecil b. demille award', 'best motion picture - drama', 'best performance by an actress in a motion picture - drama', 'best performance by an actor in a motion picture - drama', 'best motion picture - comedy or musical', 'best performance by an actress in a motion picture - comedy or musical', 'best performance by an actor in a motion picture - comedy or musical', 'best animated feature film', 'best foreign language film', 'best performance by an actress in a supporting role in a motion picture', 'best performance by an actor in a supporting role in a motion picture', 'best director - motion picture', 'best screenplay - motion picture', 'best original score - motion picture', 'best original song - motion picture', 'best television series - drama', 'best performance by an actress in a television series - drama', 'best performance by an actor in a television series - drama', 'best television series - comedy or musical', 'best performance by an actress in a television series - comedy or musical', 'best performance by an actor in a television series - comedy or musical', 'best mini-series or motion picture made for television', 'best performance by an actress in a mini-series or motion picture made for television', 'best performance by an actor in a mini-series or motion picture made for television', 'best performance by an actress in a supporting role in a series, mini-series or motion picture made for television', 'best performance by an actor in a supporting role in a series, mini-series or motion picture made for television']
+OFFICIAL_AWARDS_1819 = ['best motion picture - drama', 'best motion picture - musical or comedy', 'best performance by an actress in a motion picture - drama', 'best performance by an actor in a motion picture - drama', 'best performance by an actress in a motion picture - musical or comedy', 'best performance by an actor in a motion picture - musical or comedy', 'best performance by an actress in a supporting role in any motion picture', 'best performance by an actor in a supporting role in any motion picture', 'best director - motion picture', 'best screenplay - motion picture', 'best motion picture - animated', 'best motion picture - foreign language', 'best original score - motion picture', 'best original song - motion picture', 'best television series - drama', 'best television series - musical or comedy', 'best television limited series or motion picture made for television', 'best performance by an actress in a limited series or a motion picture made for television', 'best performance by an actor in a limited series or a motion picture made for television', 'best performance by an actress in a television series - drama', 'best performance by an actor in a television series - drama', 'best performance by an actress in a television series - musical or comedy', 'best performance by an actor in a television series - musical or comedy', 'best performance by an actress in a supporting role in a series, limited series or motion picture made for television', 'best performance by an actor in a supporting role in a series, limited series or motion picture made for television', 'cecil b. demille award']
+OFFICIAL_AWARDS = ['cecil b. demille award', 'best motion picture - drama', 'best performance by an actress in a motion picture - drama', 'best performance by an actor in a motion picture - drama', 'best motion picture - comedy or musical', 'best performance by an actress in a motion picture - comedy or musical', 'best performance by an actor in a motion picture - comedy or musical', 'best animated feature film', 'best foreign language film', 'best performance by an actress in a supporting role in a motion picture', 'best performance by an actor in a supporting role in a motion picture', 'best director - motion picture', 'best screenplay - motion picture', 'best original score - motion picture', 'best original song - motion picture', 'best television series - drama', 'best performance by an actress in a television series - drama', 'best performance by an actor in a television series - drama', 'best television series - comedy or musical', 'best performance by an actress in a television series - comedy or musical', 'best performance by an actor in a television series - comedy or musical', 'best mini-series or motion picture made for television', 'best performance by an actress in a mini-series or motion picture made for television', 'best performance by an actor in a mini-series or motion picture made for television', 'best performance by an actress in a supporting role in a series, mini-series or motion picture made for television', 'best performance by an actor in a supporting role in a series, mini-series or motion picture made for television']
+
+def get_answers(year):
+    with open('gg%sresults.json'%year, 'r') as f:
+        fres = json.load(f)
+    return fres
+
+def get_hosts(year):
+    '''Hosts is a list of one or more strings. Do NOT change the name
+    of this function or what it returns.'''
+    # Your code here
+    fres = get_answers(year)
+    hosts = fres['hosts']
+    return hosts
+
+def get_awards(year):
+    '''Awards is a list of strings. Do NOT change the name
+    of this function or what it returns.'''
+    awards = OFFICIAL_AWARDS_1315
+    return awards
+
+def get_nominees(year):
+    '''Nominees is a dictionary with the hard coded award
+    names as keys, and each entry a list of strings. Do NOT change
+    the name of this function or what it returns.'''
+    # Your code here
+    with open('gg%sanswers.json'%year, 'r') as f:
+        fres = json.load(f)
+    nominees = {award: fres['award_data'][award]['nominees'] for award in OFFICIAL_AWARDS}
+    return nominees
+
+def get_winner(year):
+    '''Winners is a dictionary with the hard coded award
+    names as keys, and each entry containing a single string.
+    Do NOT change the name of this function or what it returns.'''
+    # Your code here
+    fres = get_answers(year)
+    winners = {award: fres['award_data'][award]['winner'] for award in OFFICIAL_AWARDS_1315}
+    return winners
+
+def get_presenters(year):
+    '''Presenters is a dictionary with the hard coded award
+    names as keys, and each entry a list of strings. Do NOT change the
+    name of this function or what it returns.'''
+    # Your code here
+    fres = get_answers(year)
+    presenters = {award: fres['award_data'][award]['presenters'] for award in OFFICIAL_AWARDS_1315}
+    return presenters
+
+def pre_ceremony():
+    '''This function loads/fetches/processes any data your program
+    will use, and stores that data in your DB or in a json, csv, or
+    plain text file. It is the first thing the TA will run when grading.
+    Do NOT change the name of this function or what it returns.'''
+    # Your code here
+    return
+
+def main():
+    '''This function calls your program. Typing "python gg_api.py"
+    will run this function. Or, in the interpreter, import gg_api
+    and then run gg_api.main(). This is the second thing the TA will
+    run when grading. Do NOT change the name of this function or
+    what it returns.'''
+    # Your code here
+    gg_parser(OFFICIAL_AWARDS_1315, 'gg2013.json')
+    gg_parser(OFFICIAL_AWARDS_1315, 'gg2015.json')
+    return
+
+if __name__ == '__main__':
+    main()
